e58b94277e79ce65e9a5a3e2e05d5e201ae30a26
islamabad-optics <islamabadoptics@gmail.com>
2025-04-17 15:53:05 +0500

    yarn added

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,15 +3,13 @@ import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const API_URL = `${env.VITE_APP_BASE_NAME}`;
+  // const env = loadEnv(mode, process.cwd(), '');
+  // const API_URL = `${env.VITE_APP_BASE_NAME}`;
   const PORT = 3000;
 
   return {
     server: {
-      // this ensures that the browser opens upon server start
       open: true,
-      // this sets a default port to 3000
       port: PORT,
       host: true
     },
@@ -23,23 +21,9 @@ export default defineConfig(({ mode }) => {
       global: 'window'
     },
     resolve: {
-      alias: [
-        // { find: '', replacement: path.resolve(__dirname, 'src') },
-        // {
-        //   find: /^~(.+)/,
-        //   replacement: path.join(process.cwd(), 'node_modules/$1')
-        // },
-        // {
-        //   find: /^src(.+)/,
-        //   replacement: path.join(process.cwd(), 'src/$1')
-        // }
-        // {
-        //   find: 'assets',
-        //   replacement: path.join(process.cwd(), 'src/assets')
-        // },
-      ]
+      alias: []
     },
-    base: API_URL,
+    base: './', // ✅ this is the fix
     plugins: [react(), jsconfigPaths()]
   };
 });
